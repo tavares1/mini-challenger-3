c233e040b09d77ec1aaca9d41b807e8843226e7b
Guilherme Araujo <guiaszul@gmail.com>
2019-05-09 09:47:36 -0300

    Correct Lint
    Corrigindo projeto inicial com as regras do lint

--- minichallenger3/minichallenger3Tests/minichallenger3Tests.swift
+++ minichallenger3/minichallenger3Tests/minichallenger3Tests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import minichallenger3
 
-class minichallenger3Tests: XCTestCase {
+class MiniChallenger3Tests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
