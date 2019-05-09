6ac2562394baa7674158b7937e6a4183d3ca8789
Mateus Rodrigues <mateus.fr26@gmail.com>
2019-05-09 10:54:38 -0300

    add cloudkitmanager

---
+++ cloudkitmanager/cloudkitmanager/cloudkitmanager.h
@@ -0,0 +1,19 @@
+//
+//  cloudkitmanager.h
+//  cloudkitmanager
+//
+//  Created by Mateus Rodrigues on 09/05/19.
+//  Copyright © 2019 Mateus Rodrigues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cloudkitmanager.
+FOUNDATION_EXPORT double cloudkitmanagerVersionNumber;
+
+//! Project version string for cloudkitmanager.
+FOUNDATION_EXPORT const unsigned char cloudkitmanagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cloudkitmanager/PublicHeader.h>
+
+
---
+++ cloudkitmanager/cloudkitmanagerTests/cloudkitmanagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  cloudkitmanagerTests.swift
+//  cloudkitmanagerTests
+//
+//  Created by Mateus Rodrigues on 09/05/19.
+//  Copyright © 2019 Mateus Rodrigues. All rights reserved.
+//
+
+import XCTest
+@testable import cloudkitmanager
+
+class cloudkitmanagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
