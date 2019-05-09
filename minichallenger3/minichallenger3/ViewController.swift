6238ae25708526733afc59076e8ca27cb0287376
Guilherme Araujo <guiaszul@gmail.com>
2019-05-09 09:24:18 -0300

    swiftlint
    add swiftlint and initial project(no changes)

---
+++ minichallenger3/minichallenger3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  minichallenger3
+//
+//  Created by Guimbo on 09/05/19.
+//  Copyright © 2019 Guimbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
