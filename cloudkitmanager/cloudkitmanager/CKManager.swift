d0e93492d70b5de32afaafa579da1105fb847a94
Mateus Rodrigues <mateus.fr26@gmail.com>
2019-05-10 09:16:02 -0300

    add CKManager

---
+++ cloudkitmanager/cloudkitmanager/CKManager.swift
@@ -0,0 +1,17 @@
+//
+//  CKManager.swift
+//  cloudkitmanager
+//
+//  Created by Mateus Rodrigues on 09/05/19.
+//  Copyright © 2019 Mateus Rodrigues. All rights reserved.
+//
+
+import Foundation
+
+public final class CKManager {
+    
+    public static let shared = CKManager()
+    
+    private init() { }
+    
+}
